5cba649c52c337545238dc083b8e4444a19db9ca
Yuki KAN <re@pixely.jp>
2016-04-03 01:52:18 +0900

    add Channel Service APIs

---
+++ src/Mirakurun/api/channels/{type}/{channel}/services.ts
@@ -0,0 +1,74 @@
+/*
+   Copyright 2016 Yuki KAN
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+/// <reference path="../../../../../../typings/express/express.d.ts" />
+'use strict';
+
+import {Operation} from 'express-openapi';
+import api = require('../../../../api');
+import Channel = require('../../../../Channel');
+import Service = require('../../../../Service');
+
+export var parameters = [
+    {
+        in: 'path',
+        name: 'type',
+        type: 'string',
+        enum: ['GR', 'BS', 'CS', 'SKY'],
+        required: true
+    },
+    {
+        in: 'path',
+        name: 'channel',
+        type: 'string',
+        required: true
+    }
+];
+
+export var get: Operation = (req, res) => {
+
+    const channel = Channel.get(req.params.type, req.params.channel);
+
+    if (channel === null) {
+        api.responseError(res, 404);
+        return;
+    }
+
+    res.json(
+        Service.findByChannel(channel).map(service => service.export())
+    );
+};
+
+get.apiDoc = {
+    tags: ['channels', 'services'],
+    operationId: 'getServicesByChannel',
+    responses: {
+        200: {
+            description: 'OK',
+            schema: {
+                type: 'array',
+                items: {
+                    $ref: '#/definitions/Service'
+                }
+            }
+        },
+        default: {
+            description: 'Unexpected Error',
+            schema: {
+                $ref: '#/definitions/Error'
+            }
+        }
+    }
+};
---
+++ src/Mirakurun/api/channels/{type}/{channel}/services/{id}/stream.ts
@@ -0,0 +1,117 @@
+/*
+   Copyright 2016 Yuki KAN
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+/// <reference path="../../../../../../../../typings/express/express.d.ts" />
+'use strict';
+
+import {Operation} from 'express-openapi';
+import api = require('../../../../../../api');
+import Channel = require('../../../../../../Channel');
+import Service = require('../../../../../../Service');
+
+export var parameters = [
+    {
+        in: 'path',
+        name: 'type',
+        type: 'string',
+        enum: ['GR', 'BS', 'CS', 'SKY'],
+        required: true
+    },
+    {
+        in: 'path',
+        name: 'channel',
+        type: 'string',
+        required: true
+    },
+    {
+        in: 'path',
+        name: 'id',
+        type: 'integer',
+        maximum: 6553565535,
+        required: true
+    },
+    {
+        in: 'header',
+        name: 'X-Mirakurun-Priority',
+        type: 'integer',
+        minimum: 0
+    },
+    {
+        in: 'query',
+        name: 'decode',
+        type: 'integer',
+        minimum: 0,
+        maximum: 1
+    }
+];
+
+export var get: Operation = (req, res) => {
+
+    const channel = Channel.get(req.params.type, req.params.channel);
+
+    if (channel === null) {
+        api.responseError(res, 404);
+        return;
+    }
+
+    const service = Service.findByChannel(channel).find(sv => (sv.id === req.params.id || sv.serviceId === req.params.id));
+
+    if (!service) {
+        api.responseError(res, 404);
+        return;
+    }
+
+    let requestAborted = false;
+    req.once('close', () => requestAborted = true);
+
+    service.getStream({
+        id: (req.ip || 'unix') + ':' + (req.connection.remotePort || Date.now()),
+        priority: req.get('X-Mirakurun-Priority') || 0,
+        agent: req.get('User-Agent'),
+        disableDecoder: (req.query.decode === 0)
+    })
+        .then(stream => {
+
+            if (requestAborted === true) {
+                return stream.emit('close');
+            }
+
+            req.once('close', () => stream.emit('close'));
+
+            res.status(200);
+            stream.pipe(res);
+        })
+        .catch((err) => api.responseStreamErrorHandler(res, err));
+};
+
+get.apiDoc = {
+    tags: ['channels', 'services', 'stream'],
+    operationId: 'getServiceStreamByChannel',
+    produces: ['video/MP2T'],
+    responses: {
+        200: {
+            description: 'OK'
+        },
+        404: {
+            description: 'Not Found'
+        },
+        503: {
+            description: 'Tuner Resource Unavailable'
+        },
+        default: {
+            description: 'Unexpected Error'
+        }
+    }
+};
