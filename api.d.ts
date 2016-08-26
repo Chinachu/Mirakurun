38d1c6090103cac4cbe10ceb6c9c3c6de597b1aa
Yuki KAN <re@pixely.jp>
2016-08-27 03:49:28 +0900

    api.d.ts: added

---
+++ api.d.ts
@@ -0,0 +1,191 @@
+/*
+   Copyright 2016 Yuki KAN
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+export interface Error {
+    code?: number;
+    reason?: string;
+    errors?: ErrorOfOpenAPI[];
+}
+
+export interface ErrorOfOpenAPI {
+    errorCode?: string;
+    message?: string;
+    location?: string;
+}
+
+export type ProgramId = number;
+
+export type EventId = number;
+
+export type ServiceId = number;
+
+export type NetworkId = number;
+
+export type ServiceItemId = number;
+
+export type UnixtimeMS = number;
+
+export interface Channel {
+    type: ChannelType;
+    channel: string;
+    name?: string;
+    satelite?: string;
+    services?: Service[];
+}
+
+export type ChannelType = "GR" | "BS" | "CS" | "SKY";
+
+export interface Service {
+    id: ServiceItemId;
+    serviceId: ServiceId;
+    networkId: NetworkId;
+    name: string;
+    logoId?: number;
+    hasLogoData?: boolean;
+    channel?: Channel;
+}
+
+export interface Program {
+    id: ProgramId;
+    eventId: EventId;
+    serviceId: ServiceId;
+    networkId: NetworkId;
+    startAt: UnixtimeMS;
+    duration: number;
+    isFree: boolean;
+
+    name?: string;
+    description?: string;
+    genres?: ProgramGenre[];
+    video?: {
+        type: ProgramVideoType;
+        resolution: ProgramVideoResolution;
+        streamContent: number;
+        componentType: number;
+    }
+    audio?: {
+        samplingRate: ProgramAudioSamplingRate;
+        componentType: number;
+    }
+    relatedItems?: ProgramRelatedItem[];
+}
+
+export interface ProgramGenre {
+    lv1: number;
+    lv2: number;
+    un1: number;
+    un2: number;
+}
+
+export type ProgramVideoType = "mpeg2" | "h.264" | "h.265";
+
+export type ProgramVideoResolution = "240p" | "480i" | "480p" | "720p" | "1080i" | "2160p" | "4320p";
+
+export enum ProgramAudioSamplingRate {
+    "16kHz" = 16000,
+    "22.05kHz" = 22050,
+    "24kHz" = 24000,
+    "32kHz" = 32000,
+    "44.1kHz" = 44100,
+    "48kHz" = 48000
+}
+
+export interface ProgramRelatedItem {
+    networkId?: number;
+    serviceId: number;
+    eventId: number;
+}
+
+export interface TunerDevice {
+    index: number;
+    name: string;
+    types: ChannelType;
+    command: string;
+    pid: number;
+    users: TunerUser[];
+    isAvailable: boolean;
+    isFree: boolean;
+    isUsing: boolean;
+}
+
+export interface TunerUser {
+    id: string;
+    priority: number;
+    agent?: string;
+}
+
+export interface Event {
+    resource: EventResource;
+    type: EventType;
+    data: any;
+    time: UnixtimeMS;
+}
+
+export type EventResource = "program" | "service" | "tuner";
+
+export type EventType = "create" | "update" | "redefine";
+
+export interface ConfigServer {
+    path?: string;
+    port?: number;
+    logLevel?: LogLevel;
+}
+
+export enum LogLevel {
+    "FATAL" = -1,
+    "ERROR" = 0,
+    "WARN" = 1,
+    "INFO" = 2,
+    "DEBUG" = 3
+}
+
+export type ConfigTuners = ConfigTunersItem[];
+
+export interface ConfigTunersItem {
+    name: string;
+    types: ChannelType[];
+    /** for chardev / dvb */
+    command?: string;
+    /** for dvb */
+    dvbDevicePath?: string;
+    decoder?: string;
+    /** special flag */
+    isPT2?: boolean;
+    isDisabled?: boolean;
+}
+
+export type ConfigChannels = ConfigChannelsItem[];
+
+export interface ConfigChannelsItem {
+    name: string;
+    type: ChannelType;
+    /** passed to tuning command */
+    channel: string;
+    satelite?: string;
+    serviceId?: number;
+    isDisabled?: boolean;
+}
+
+export interface Version {
+    current: string;
+    latest: string;
+}
+
+export interface Status {
+    epg: {
+        gatheringNetworks: NetworkId[];
+    }
+}
