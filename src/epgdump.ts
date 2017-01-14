17dda400696d746e4d087e1285fb22d764e795a6
Yuki KAN <re@pixely.jp>
2017-01-14 09:08:22 +0900

    mirakurun-epgdump: added

---
+++ src/epgdump.ts
@@ -0,0 +1,110 @@
+/*
+   Copyright 2017 Yuki KAN
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+/// <reference path="../typings/index.d.ts" />
+"use strict";
+
+import * as stream from "stream";
+import * as fs from "fs";
+
+if (process.argv.length < 3) {
+    console.error("Mirakurun EPG Dump Test Program");
+    console.error("Usage: mirakurun-epgdump src.ts dest.json");
+    process.exit(1);
+}
+
+const force = process.argv[process.argv.length - 3] === "-f";
+const src = process.argv[process.argv.length - 2];
+const dest = process.argv[process.argv.length - 1];
+
+console.log("src:", src);
+console.log("dest:", dest);
+
+if (fs.existsSync(src) === false) {
+    console.error(`"${src}" is not exists.`);
+    process.exit(1);
+}
+if (fs.existsSync(dest) === true && force === false) {
+    console.error(`"${dest}" is exists.`);
+    process.exit(1);
+}
+
+process.env.SERVER_CONFIG_PATH = "./config/server.yml";
+process.env.PROGRAMS_DB_PATH = dest;
+
+const aribts = require("aribts");
+import _ from "./Mirakurun/_";
+import Event from "./Mirakurun/Event";
+import Program from "./Mirakurun/Program";
+import epg from "./Mirakurun/epg";
+import * as config from "./Mirakurun/config";
+import * as log from "./Mirakurun/log";
+
+log.logLevel = log.LogLevel.INFO;
+_.config.server = config.loadServer();
+new Event();
+new Program();
+
+const size = fs.statSync(src).size;
+let bytesRead = 0;
+let events = 0;
+
+const tsStream: stream.Transform = new aribts.TsStream();
+const readStream = fs.createReadStream(src);
+
+const transformStream = new stream.Transform({
+    transform: function(chunk, encoding, done) {
+
+        bytesRead += chunk.length;
+
+        console.log("\u001b[2A");
+        console.log(`reading - ${bytesRead} of ${size} [${Math.floor(bytesRead / size * 100)}%] (events=${events})`);
+
+        this.push(chunk);
+        done();
+    },
+    flush: done => {
+
+        console.log("\u001b[2A");
+        console.log(`reading - ${bytesRead} of ${size} [${Math.floor(bytesRead / size * 100)}%] (events=${events}) [done]`);
+        console.timeEnd("read");
+
+        setTimeout(finalize, 3500);
+        done();
+    }
+});
+
+console.log("");
+console.time("read");
+readStream.pipe(transformStream);
+transformStream.pipe(tsStream);
+
+tsStream.on("eit", (pid, data) => {
+    epg.write(data);
+    events = _.program.items.length;
+});
+tsStream.resume();
+
+function finalize() {
+
+    const programs = _.program.items;
+
+    console.log("programs:", programs.length, "(events)");
+
+    fs.writeFileSync(dest, JSON.stringify(programs.map(program => program.data), null, "  "));
+
+    console.log(`saved to "${dest}".`);
+    process.exit(0);
+}
