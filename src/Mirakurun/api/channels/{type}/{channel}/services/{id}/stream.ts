4b48f79e6302cb3c9e0055ee1aa157edf83fda38
Yuki KAN <re@pixely.jp>
2018-10-19 03:54:32 +0900

    api/*/stream: use parseInt before use X-Mirakurun-Priority

--- src/Mirakurun/api/channels/{type}/{channel}/services/{id}/stream.ts
+++ src/Mirakurun/api/channels/{type}/{channel}/services/{id}/stream.ts
@@ -75,7 +75,7 @@ export const get: Operation = (req, res) => {
 
     service.getStream({
         id: (req.ip || "unix") + ":" + (req.connection.remotePort || Date.now()),
-        priority: req.get("X-Mirakurun-Priority") || 0,
+        priority: parseInt(req.get("X-Mirakurun-Priority"), 10) || 0,
         agent: req.get("User-Agent"),
         disableDecoder: (req.query.decode === 0)
     })
