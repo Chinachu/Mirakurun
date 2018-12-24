c546a17d8b0c42f5fc603e68cff346f2ab8b9667
Yuki KAN <re@pixely.jp>
2018-12-25 02:32:32 +0900

    remove unnecessary dependency (colors)

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -23,7 +23,6 @@ const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
 const semver = require("semver");
-const colors = require("colors");
 const pkg = require("../package.json");
 
 // node check
