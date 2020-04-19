864b2da6917cd84b43ac2fe81653bac7b4ca2cbb
Yuki KAN <re@pixely.jp>
2020-04-19 18:09:39 +0900

    update copyright (only comment)

--- bin/munin-plugins/mirakurun_status.js
+++ bin/munin-plugins/mirakurun_status.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
-   Copyright 2016 Yuki KAN
+   Copyright 2016 kanreisa
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
