1ad8c5b661b33aaa2ed3d8c82c28b66e5b2bf89e
kanreisa <re@pixely.jp>
2020-07-08 23:32:14 +0900

    docker/container-init.sh: rename wrong filename (<= 3.1.1 >= 3.0.0)

--- docker/container-init.sh
+++ docker/container-init.sh
@@ -5,14 +5,22 @@ export LOG_LEVEL=${LOG_LEVEL:-"2"}
 export SERVER_CONFIG_PATH=/app-config/server.yml
 export TUNERS_CONFIG_PATH=/app-config/tuners.yml
 export CHANNELS_CONFIG_PATH=/app-config/channels.yml
-export SERVICES_DB_PATH=/app-data/services.yml
-export PROGRAMS_DB_PATH=/app-data/programs.yml
+export SERVICES_DB_PATH=/app-data/services.json
+export PROGRAMS_DB_PATH=/app-data/programs.json
 
 export PATH=/opt/bin:$PATH
 export DOCKER=YES
 
 if [ ! -e "/opt/bin" ]; then
   mkdir -pv /opt/bin
+fi
+
+# rename wrong filename (migration from <= 3.1.1 >= 3.0.0)
+if [ -f "/app-data/services.yml" -a ! -f "$SERVICES_DB_PATH" ]; then
+  cp -v "/app-data/services.yml" "$SERVICES_DB_PATH"
+fi
+if [ -f "/app-data/programs.yml" -a ! -f "$PROGRAMS_DB_PATH" ]; then
+  cp -v "/app-data/programs.yml" "$PROGRAMS_DB_PATH"
 fi
 
 # only for test purpose
