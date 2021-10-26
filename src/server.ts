016aabbece85117fc379e853af7e234f239aedce
kanreisa <re@pixely.jp>
2021-10-27 00:29:10 +0900

    server: (testing) disable memory pool of `Buffer`

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,8 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+Buffer.poolSize = 0; // disable memory pool
+
 require("dotenv").config();
 import { execSync } from "child_process";
 import { createHash } from "crypto";
