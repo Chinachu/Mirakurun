ca92346eca0b6414ece89c5dd6221863d1e49b0c
kanreisa <re@pixely.jp>
2022-01-10 16:14:02 +0900

    remove `colors` package [urgent/security-fix] (backport)

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -23,14 +23,13 @@ const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
 const semver = require("semver");
-const colors = require("colors");
 const pkg = require("../package.json");
 
 // node check
 if (semver.satisfies(process.version, pkg.engines.node) === true) {
-    console.log("Version:", `node@${process.version}`.green, "[OK]".bgGreen);
+    console.log("Version:", `node@${process.version}`, "[OK]");
 } else {
-    console.error("Version:", `node@${process.version}`.red, "[NG]".bgRed, "Expected:", pkg.engines.node);
+    console.error("Version:", `node@${process.version}`, "[NG]", "Expected:", pkg.engines.node);
     process.exit(1);
 }
 
@@ -57,9 +56,9 @@ if (process.platform === "linux" || process.platform === "darwin") {
     const pm2Version = child_process.execSync("pm2 -v", { encoding: "utf8" }).trim();
     const pm2Expected = ">=2.4.0";
     if (semver.satisfies(pm2Version, pm2Expected) === true) {
-        console.log("Version:", `pm2@${pm2Version}`.green, "[OK]".bgGreen);
+        console.log("Version:", `pm2@${pm2Version}`, "[OK]");
     } else {
-        console.error("Version:", `pm2@${pm2Version}`.red, "[NG]".bgRed, "Expected:", pm2Expected);
+        console.error("Version:", `pm2@${pm2Version}`, "[NG]", "Expected:", pm2Expected);
         process.exit(1);
     }
 
@@ -72,7 +71,7 @@ if (process.platform === "linux" || process.platform === "darwin") {
             ]
         });
     } catch (e) {
-        console.log("Caution: `pm2 startup` has failed. you can try fix yourself.".bgYellow);
+        console.log("Caution: `pm2 startup` has failed. you can try fix yourself.");
     }
 
     child_process.execSync("pm2 start processes.json", {
@@ -95,9 +94,9 @@ if (process.platform === "linux" || process.platform === "darwin") {
     const winserVersion = child_process.execSync("winser -v", { encoding: "utf8" }).replace(/^[a-z]+ /, "").trim();
     const winserExpected = ">=1.0.3 <2.0.0";
     if (semver.satisfies(winserVersion, winserExpected) === true) {
-        console.log("Version:", `winser@${winserVersion}`.green, "[OK]".bgGreen);
+        console.log("Version:", `winser@${winserVersion}`, "[OK]");
     } else {
-        console.error("Version:", `winser@${winserVersion}`.red, "[NG]".bgRed, "Expected:", winserExpected);
+        console.error("Version:", `winser@${winserVersion}`, "[NG]", "Expected:", winserExpected);
         process.exit(1);
     }
 
